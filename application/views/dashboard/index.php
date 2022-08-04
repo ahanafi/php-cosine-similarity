292e452840293ce52c8d2583d05f8371bb704578
Ahmad Hanafi <ahanafi.id@gmail.com>
2022-08-05 06:02:49 +0700

    Change bg home

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -38,18 +38,37 @@
         </div>
         <div class="row">
             <div class="col-lg-12 col-12">
-                <div class="card">
-                    <div class="card-header">
+                <div class="card" id="main-card">
+                    <div class="card-header border-0">
                         <h4>Halaman Dashboard</h4>
                     </div>
-                    <div class="card-body d-flex justify-content-center" style="min-height:400px;">
-                        Selamat datang di Sistem Deteksi Kemiripan Judul Skripsi Pada Program Studi Sistem Informasi      
-                    </div>
-                    <div class="card-body d-flex justify-content-center" style="min-height: 280px;">
-                        Selamat datang di Sistem Deteksi Kemiripan Judul Skripsi Pada Program Studi Sistem Informasi      
+                    <div class="card-body d-flex justify-content-center align-items-center" style="min-height:400px;">
+                        <p>
+                            Selamat datang di Sistem Deteksi Kemiripan Judul Skripsi <br> Pada Program Studi Sistem Informasi
+                        </p>
                     </div>
                 </div>                
             </div>
         </div>
     </section>
 </div>
+<style>
+    #main-card {
+        color: #fff;
+        background-color: #0C1021;
+        background-image: url('<?php echo base_url('assets/img/photo21.jpg') ?>');
+        background-size: cover;
+        background-position: 0 50%;
+    }
+
+    #main-card div{
+        background-color: rgba(5, 77, 158, 0.85) !important;
+    }
+
+    #main-card .card-body p {
+        font-weight: bolder;
+        text-align: center;
+        font-style: italic;
+        font-size: 1.5rem;
+    }
+</style>
